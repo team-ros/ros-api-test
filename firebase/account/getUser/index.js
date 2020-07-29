a090e7697e93ad176c40b3dfece9671953c84f4a
Alexander Gauss <gauss@Alexanders-MacBook-Pro.local>
2020-07-29 21:55:04 +0200

    added a view where you can copy the token

--- firebase/account/getUser/index.js
+++ firebase/account/getUser/index.js
@@ -1,10 +1,11 @@
 import firebase from '../../connection'
+import 'firebase/auth'
 
 export default () => {
     return new Promise( data => {
         data({
             status: true,
-            data: firebase.auth().currentUser
+            data: firebase.auth().currentUser.getIdToken()
         })
     })
 }
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,31 @@
 <template>
-    <h1> Dashboard </h1>
+    <div>
+        <h1> Dashboard </h1>
+        <p>Token:</p> <textarea type="text" v-model="userdata" />
+    </div>
 </template>
 
 <script>
-export default {
+    import firebase from '../firebase/account/getUser'
 
-}
+    export default {
+        data() {
+            return {
+                userdata: ""
+            }
+        },
+        mounted() {
+            firebase()
+            .then( data => {
+                if( data.status ) {
+                    this.userdata = data.data.i 
+                }
+            })
+            .catch( err => {
+                console.log( err )
+            })
+        }
+    }
 </script>
 
 <style>
